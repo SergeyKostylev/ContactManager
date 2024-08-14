2d3a2a1f19f861ae4d932c918a814d3b6fca9472
NutSheLL <holnaz1337@gmail.com>
2024-08-14 20:17:02 +0300

    Changed validation for birthday

--- src/services/validator.py
+++ src/services/validator.py
@@ -1,5 +1,6 @@
 import re
 from src.services.console_text_designer import ConsoleTextDesigner as console_des
+from datetime import datetime, timedelta
 
 def email(email: str) -> bool:
     email_regex = r"(^[\w\.-]+@[\w\.-]+\.\w+$)"
@@ -21,7 +22,27 @@ def validate_user_name(user_name: str) -> bool:
     return re.match(username_regex, user_name) is not None
 
 
-def birthday(birthday: str) -> bool:
-    birthday_regex = r"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$"
+def validate_birthday(birthday: str) -> bool:
+    # DD.MM.YYYY
+    birthday_regex = r"^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.\d{4}$"
+    
+  
     if not re.match(birthday_regex, birthday):
+        return False
+    
+    try:
+        
+        birthday_date = datetime.strptime(birthday, '%d.%m.%Y')
+        
+        # Get today's date
+        today = datetime.today()
+            
+        if birthday_date > today:
+            return False
+            
+        if birthday_date < today - timedelta(days=365*100):
+            return False
+        
+        return True
+    except ValueError:
         return False
